b48af4d89c58a68bb09fa8a9b7c0e4bd28224b5a
Ankita Hrishikesh Upadhyay <31573973+leoankita@users.noreply.github.com>
2018-01-08 20:45:41 -0600

    Assignment7

--- z1836412.sql
+++ z1836412.sql
@@ -1,96 +1,77 @@
-/*Assignment Number : 5
+/*Assignment Number : 7
   Student Name      : Ankita Upadhyay
   Student Z-ID      : Z1836412
 */
 
-/* 1. SQL Query to list employees working in each city*/
-select count(e.employeeNumber)Number_of_Employees_Working,o.city 
-from Employees e,Offices o 
-where o.officeCode=e.officeCode 
-group by o.city 
-order by o.city;
+/*2. creating a table owner with an owner id and a name. 
+Owner id is an auto-increment primary key*/
+create table Owner 
+(ownerID int NOT NULL AUTO_INCREMENT , 
+ownerName varchar(255) NOT NULL , 
+PRIMARY KEY (ownerID));
 
-/* 2. SQL Query to List each employee first name and last name and the number of customers for each one.*/
-select e.firstName,e.lastName,count(c.customerNumber)Number_of_Customers 
-from Employees e,Customers c 
-where c.salesRepEmployeeNumber=e.employeeNumber 
-group by e.employeeNumber 
-order by e.firstName;
-
-/* 3. SQL Query to List each employee first name and last name and the first and last name of the person that employee reports to.*/
-select e.firstName Employee_First_Name,e.lastName Employee_Last_Name,
-e1.firstName Reports_To_First_Name,e1.lastName Reports_To_Last_Name 
-from Employees e,Employees e1 
-where e.reportsTo=e1.employeeNumber 
-order by e.firstName;
-
-/* 4. SQL Query to list the contact person (first name and last name) and the total amount of payments for first 25 customers.*/
-select c.contactFirstName,c.contactLastName,sum(p.amount) Total_Amount 
-from Customers c, Payments p 
-where c.customerNumber=p.customerNumber 
-group by c.CustomerNumber  
-order by c.customerNumber 
-LIMIT 25;
-
-/* 5. SQL Query to list How many customers live in the same city as their sales rep works*/
-select count(c.customerNumber)Number_of_Customers_who_live_in_same_city_as_their_Sale_Rep_work  
-from Customers c,Employees e, Offices o 
-where o.officeCode=e.officeCode 
-and o.city=c.city;
-
-/* 6. SQL Query to list How many customers live in the same city as their sales rep works,listing the name of the city and the number of customers.*/
-select count(c.customerNumber)Number_of_Customers_who_live_in_same_city_as_their_Sale_Rep_work ,o.city 
-from Customers c,Employees e, Offices o 
-where o.officeCode=e.officeCode 
-and o.city=c.city 
-group by c.customerNumber 
-order by o.city;
-
-/* 7. SQL Query to list Which customer (just the customer name) has ordered the most expensive product (based on the buyPrice)*/
-select c.CustomerName 
-from Customers c, Products p , OrderDetails od,Orders o 
-where od.productCode=p.productCode 
-and od.orderNumber=o.orderNumber 
-and o.customerNumber=c.customerNumber 
-and p.buyPrice=
-(select MAX(buyPrice) from Products);
-
-/* 8. SQL Query to list Which customer has made the largest payment? list just the customer name.*/
-select c.customerName 
-from Customers c, Payments p 
-where c.customerNumber=p.customerNumber 
-and p.amount=
-(select MAX(amount) from Payments);
-
-/* 9. i) SQL Query to List all of the product descriptions for products from Min LinDiecast and Exoto Designs */
-select productDescription 
-from Products 
-where productVendor='Min Lin Diecast' 
-OR productVendor='Exoto Designs';
-
-/* 9. ii) SQL Query to List all of the product descriptions for products from Min LinDiecast and Exoto Designs */
-select productDescription 
-from Products 
-where productVendor 
-IN ('Min Lin Diecast','Exoto Designs');
+/*3. inserting at least 5 records in this table (inserted 9 records)*/
+INSERT INTO Owner 
+(ownerName) 
+Values 
+('Diana'),('Ankita'),('Piyush'),
+('Amit'),('Ekta'),('Harold'),
+('Gwen'),('Benny'),('John');
 
 
-/*Extra credits 1. SQL Query to list the order number, the customer name and all of the product names on that order in ascending order of customer name for first 10 orders*/
-select o.orderNumber,c.customerName,group_concat(distinct p.productName) 
-from Orders o,Customers c,Products p ,OrderDetails od  
-where o.customerNumber=c.customerNumber  
-and od.productCode=p.productCode   
-and od.orderNumber=o.orderNumber 
-group by o.orderNumber  
-order by customerName  
-LIMIT 10;
+/*4. select * on Owner table to show all the records*/
+Select * from Owner;
 
-/*Extra credits 2. SQL Query to list the average dollar amount for each order*/
-select o.orderNumber,avg(p.amount) 
-from Payments p,Orders o,Customers c 
-where c.customerNumber=o.customerNumber 
-and o.customerNumber=p.customerNumber 
-group by o.orderNumber 
-order by o.orderNumber;
+/*5. creating a table called pet with a pet id (auto-increment
+primary key), pet name and owner-id which is a foreign key
+into the owner table*/
+create table Pet 
+(petID int NOT NULL AUTO_INCREMENT , 
+petName varchar(255) NOT NULL, 
+ownerID int NOT NULL , 
+PRIMARY KEY (PETID),
+FOREIGN KEY (ownerID) 
+REFERENCES Owner(ownerID));
+ 
+/*6.inserting at least 5 records in this table, with at least two
+pets owned by the same owner(inserted 11 records)*/ 
+INSERT INTO Pet 
+(petName,ownerID) 
+Values ('Tommy',1),('Fluffy',2),('Claws',2),
+('Buffy',3),('Fang',3),('Whistler',4),
+('Browser',5),('Chirpy',6),('Slim',7),
+('Abby',8),('Arrow',9);
+
+/*7. selecting records from Pet table to show all the records*/
+select * from Pet;
+
+/*8. adding a column to the pet table for type of pet (for example:
+dog, cat, or fish)*/
+ALTER TABLE Pet ADD petType varchar(50);
+
+
+/*9. updating several rows to add the pet type*/
+UPDATE Pet SET petType='dog' where petID IN (1,4,5,7);
+UPDATE Pet SET petType='cat' where petID IN (2,3,10);
+UPDATE Pet SET petType='bird' where petID IN (6,8,9);
+UPDATE Pet SET petType='fish' where petID=11;
+
+/*10. selecting records from Pet table to show all the records*/
+select * from Pet;
+
+/*11. creating a view that will list each owner with their
+pet, just the names*/
+CREATE VIEW PetOwner 
+AS select o.ownerName, p.petName 
+from Owner o, Pet p 
+where o.ownerID=p.ownerID;
+
+/*12. selecting on this view to show all the records*/
+select * from PetOwner;
+
+/*1. droping all of the tables/views created above*/
+Drop table Pet,Owner;
+Drop view PetOwner;
+
 
 
